aa2d7aa9c4d6b471eccde755bada1e85275068ef
Brooke Lin <brookelin@Brookes-MacBook-Pro.local>
2025-10-03 10:33:08 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+console.clear();
+
+// interactive with rating numbers
+let ratingButton = document.querySelectorAll(".rating-button-item");
+let selectedRate;
+let givenRate = document.querySelector("#given-rate");
+
+  ratingButton.forEach((btn,index)=>{
+    btn.addEventListener('click',(e)=>{
+      let currentSelection = e.target.dataset.rate;
+      console.log(`selectedR:${selectedRate}, CurrentSelection: ${currentSelection},${index}`);
+      
+        // return early if the number is selected already
+    if(selectedRate && selectedRate == currentSelection) return;
+      
+      // Otherwise  remove selected class
+      if(selectedRate){
+        ratingButton[Number(selectedRate)-1].classList.remove('selected');
+      }
+      
+      //add class to selected button/number
+      ratingButton[currentSelection-1].classList.add('selected');
+      
+      // Then set current selection as selected rate
+      selectedRate = currentSelection;
+      
+      // The givenRate will be changed when selectedRate is change although it is not display on the screen
+      givenRate.innerHTML = selectedRate;
+      console.log(givenRate);
+          
+      // Stop the event from bubbling up
+      e.stopPropagation(); 
+    })
+    
+  })
+
+
+
+// change page information when hit the submit button
+const RatingPage = document.querySelector(".rating-page");
+const SubmittedPage = document.querySelector(".submitted-page");
+const SumitFormButton = document.querySelector("button.submit-button");
+
+
+SumitFormButton.addEventListener('click',function(){
+  pageRendering(true);
+  
+})
+
+function pageRendering(isSubmitted=false){
+  if(!isSubmitted) return 
+  
+  if(isSubmitted){
+    RatingPage.classList.add('hidden');
+    SubmittedPage.classList.remove('hidden');
+  }
+}
